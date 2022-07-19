c04b97cd1006ca5d4f06f1bdac1a81e5fa5069fe
danielpqb <danielpqb@gmail.com>
2022-07-19 18:03:05 -0300

    Screen 02 - Questions

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,30 @@
+import React from "react"
+
 export default function Question(props) {
+    const [questionStateClass, setQuestionStateClass] = React.useState('')
+
+    function changeQuestionStateClass() {
+        switch (questionStateClass) {
+            case '':
+                setQuestionStateClass('opened')
+                break
+            case 'opened':
+                setQuestionStateClass('revealed')
+                break
+            case 'revealed':
+                setQuestionStateClass('answered')
+                break
+            default:
+                setQuestionStateClass('')
+                break
+        }
+    }
+
+    const [questionContent, setQuestionContent] = React.useState()
+
     return (
-        <div className="question" onClick={() => { }}>
-            Pergunta {props.id}
+        <div className={"question " + questionStateClass} onClick={() => { changeQuestionStateClass() }}>
+            {"Pergunta " + props.id}
             <ion-icon name="play-outline"></ion-icon>
         </div>
     )
