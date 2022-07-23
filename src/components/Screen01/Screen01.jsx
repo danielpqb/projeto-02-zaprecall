2209fb8c912f5c6580a428d9c6d6bf1bbdb7e035
danielpqb <danielpqb@gmail.com>
2022-07-23 01:26:21 -0300

    Screen 01

--- src/components/Screen01/Screen01.jsx
+++ src/components/Screen01/Screen01.jsx
@@ -8,6 +8,14 @@ export default function Screen01() {
         <div className="screen01">
             <ZapRecallIcon />
             <ZapRecallLogo />
+            <select className="chooseDeck" onChange='{changeDeck}' required>
+                <option className="placeholder" value="0" selected hidden disabled>Escolha seu deck</option>
+                <option value="1">React</option>
+                <option value="2">QQQQQ</option>
+            </select>
+            <input className="zapGoal" placeholder="Digite sua meta de zaps...">
+
+            </input>
             <BeginButton />
         </div>
     )
--- src/components/ZapRecallIcon/ZapRecallIcon.jsx
+++ src/components/ZapRecallIcon/ZapRecallIcon.jsx
@@ -1,10 +1,10 @@
-import logo from '../../assets/img/zapIcon.svg'
+import icon from '../../assets/img/zapIcon.svg'
 import './style.css'
 
 export default function ZapRecallIcon() {
     return (
         <div className="zapIcon">
-            <img src={logo} alt="" />
+            <img src={icon} alt="" />
         </div>
     )
 }
